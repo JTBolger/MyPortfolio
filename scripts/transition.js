cb0308bdba34c10d3d3f23a5ee2b3799b2d2ac8e
JTBolger <112192375+JTBolger@users.noreply.github.com>
2022-10-22 21:23:10 -0500

    redesign
    got more inspiration. out with the old and in with the new

---
+++ scripts/transition.js
@@ -0,0 +1,21 @@
+window.onload = function() {loadIntro()}
+window.onload = function() {moveBG()}
+window.onload = function() {moveBottom()}
+window.onload = function() {titleIntro()}
+
+var title = document.getElementById("PageTitle")
+var titleFG = document.getElementById("transitionFG")
+var bottom = document.getElementById("bottom")
+
+function loadIntro() {
+    title.style.opacity = 1;
+}
+function moveFG() {
+    titleFG.style.height = "150%"
+    titleFG.style.borderRadius = "50%"
+    titleFG.style.translate = "0px -1000px"
+    bottom.style.height = "15%";
+    title.style.opacity = 0;
+}
+setTimeout(moveFG, 700);
+setTimeout(loadIntro, 100);
