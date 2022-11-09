02a4f59e3b40beae97596d6ba62ab85293c05a0e
JTBolger <112192375+JTBolger@users.noreply.github.com>
2022-11-09 01:04:13 -0600

    Updated graph
    Updated the graph to be fully functional

---
+++ scripts/graph.js
@@ -0,0 +1,58 @@
+function menu() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.options')) {
+        var dropdowns = document.getElementsByClassName("options-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+var bar1 = document.getElementById("bar1")
+var bar1Text = document.getElementById("bar1Text")
+var bar2 = document.getElementById("bar2")
+var bar2Text = document.getElementById("bar2Text")
+var bar3 = document.getElementById("bar3")
+var bar3Text = document.getElementById("bar3Text")
+var bar4 = document.getElementById("bar4")
+var bar4Text = document.getElementById("bar4Text")
+var bar5 = document.getElementById("bar5")
+var bar5Text = document.getElementById("bar5Text")
+var bar6 = document.getElementById("bar6")
+var bar6Text = document.getElementById("bar6Text")
+
+function portfolioGraph() {
+    bar1.style.height = "14500%"
+    bar1Text.textContent = "33.0%"
+    bar2.style.height = "8500%"
+    bar2Text.textContent = "61.1%"
+    bar3.style.height = "1750%"
+    bar3Text.textContent = "5.9%"
+    bar4.style.height = "750%"
+    bar4Text.textContent = ""
+    bar5.style.height = "750%"
+    bar5Text.textContent = ""
+    bar6.style.height = "750%"
+    bar6Text.textContent = ""
+}
+function afterhoursGraph() {
+    bar1.style.height = "1000%"
+    bar1Text.textContent = "0.9%"
+    bar2.style.height = "7000%"
+    bar2Text.textContent = "30.7%"
+    bar3.style.height = "9250%"
+    bar3Text.textContent = "44.3%"
+    bar4.style.height = "4800%"
+    bar4Text.textContent = "22.9%"
+    bar5.style.height = "750%"
+    bar5Text.textContent = ""
+    bar6.style.height = "750%"
+    bar6Text.textContent = ""
+}
